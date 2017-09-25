e678065118596dc2ac3e919de66227bac772ea78
NULL-SWEAT <felipe.094@gmail.com>
2017-09-25 06:01:03 -0300

    Changed repository link.

--- src/components/Start.js
+++ src/components/Start.js
@@ -12,8 +12,7 @@ class Start extends Component {
 					<img style={{ height: '2rem' }} src={'https://avatars1.githubusercontent.com/u/7648832'} alt="Kitsu API" />
 					<span style={{ marginLeft: '1rem' }}>Powered by Kitsu API</span>
 				</a>
-			{/* TROCAR O LINK */}
-				<a href={'http://docs.kitsu.apiary.io/'} target="_blank" className="text-light btn btn-dark btn-block">
+				<a href={'https://github.com/NULL-SWEAT/SADb'} target="_blank" className="text-light btn btn-dark btn-block">
 					<img src={require('../GitHub-Mark-32px.png')} alt="Github repo" />
 					<span style={{ marginLeft: '1rem' }}>View Code</span>
 				</a>
